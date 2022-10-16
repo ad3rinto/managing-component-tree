08a2e9f93e5138ad1390e7e23aca5525f689b8da
Micheal A <aad3rinto@gmail.com>
2022-10-16 20:39:19 +0000

    add yaml file

---
+++ build/precache-manifest.ee865e3d39071eea8e07dc3c7231c02c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edfd62daa957dda9fe6e9bd9ca445dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6776cba66e622debbaf3",
+    "url": "/static/js/2.993384cc.chunk.js"
+  },
+  {
+    "revision": "518bb882412673cf05a9",
+    "url": "/static/js/main.0860f5f4.chunk.js"
+  },
+  {
+    "revision": "c415adb34cc05b8216ce",
+    "url": "/static/js/runtime-main.81c4a991.js"
+  }
+]);
